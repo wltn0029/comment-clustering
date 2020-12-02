28ed01de0d2643556f7bae35c939aee7b30cb67e
zinuok <zinuok@kaist.ac.kr>
2020-12-02 18:29:31 +0900

    API server

--- backend/model_server.py
+++ backend/model_server.py
@@ -7,14 +7,14 @@ app = Flask(__name__)
 
 @app.route("/")
 def model_api():
-    return "<h1>TEST!</h1>"
+    return "<h1>This is a TEST!</h1>"
 
 
 
+# for external access) http://143.248.144.129:8080/
 if __name__ == '__main__':
     # options
-    #host_addr = "192.168.0.100"
-    host_addr = '0.0.0.0'
+    host_addr = '0.0.0.0' # broadcast to network
     port_num = 8080
     app.debug == True
 
